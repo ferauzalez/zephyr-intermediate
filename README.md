54563e28d2fd4f89a7b802793b78c944a3374e76
ferauzalez <ferauzalez@gmail.com>
2026-09-25 11:38:04 -0300

    add homework for lesson 2

--- README.md
+++ README.md
@@ -1,42 +1,21 @@
-I make a test with the three taks with differents priority levels. With 1516 samples I observe the following count appearences:
-* T_LOW:	312 times
-* T_MED:	467 times
-* T_HIGH:	936 times
-  So, T_HIGH was the thread that ran more often.
-  
-In order to see if priority does affect interleaving, I watched again the demo of interleaving. After refreshing these concepts I can say that indeed priority affects interleaving helping the scheduler decide what thread run when two task are ready.
+I ran the l2-demo in order to observe a race condition and I got the following output, detecting the race condition:
 
-And finnaly I paste two outputs:
-* The following is a sample of the homework with the propose message:
-T_HIGH running
-T_MED running
-T_HIGH running
-T_LOW running
-T_HIGH running
-T_MED running
-T_HIGH running
-T_HIGH running
-T_LOW running
-T_MED running
-T_HIGH running
-T_HIGH running
-
-* The following is a sample of the homework with the fourth cooperative thread and a time elapsed message:
+```bash
 *** Booting Zephyr OS build v4.4.0 ***
-Cooperative thread running. Iteration number 0.
-Cooperative thread running. Iteration number 1.
-Cooperative thread running. Iteration number 2.
-Cooperative thread running. Iteration number 3.
-Cooperative thread running. Iteration number 4.
-T_HIGH running. Time elapsed: 0 ms
-T_MED running. Time elapsed: 0 ms
-T_LOW running. Time elapsed: 0 ms
-T_HIGH running. Time elapsed: 100 ms
-T_MED running. Time elapsed: 200 ms
-T_HIGH running. Time elapsed: 200 ms
-T_LOW running. Time elapsed: 300 ms
-T_HIGH running. Time elapsed: 300 ms
-T_MED running. Time elapsed: 400 ms
-T_HIGH running. Time elapsed: 400 ms
-T_HIGH running. Time elapsed: 500 ms
+[00:00:00.000,000] <inf> demo: Expected final value: 2000000
+[00:00:00.163,000] <inf> demo: [worker_a] finished
+[00:00:00.167,000] <inf> demo: [worker_b] finished
+[00:00:00.167,000] <inf> demo: Actual final value: 1520645
+[00:00:00.167,000] <err> demo: Race condition confirmed: lost 479355 updates
+```
+And after adding a mutex, the race condition was avoided:
 
+```bash
+*** Booting Zephyr OS build v4.4.0 ***
+[00:00:00.000,000] <inf> demo: START
+[00:00:00.000,000] <inf> demo: Expected final value: 2000000
+[00:00:40.880,000] <inf> demo: [worker_a] finished
+[00:00:40.880,000] <inf> demo: [worker_b] finished
+[00:00:40.880,000] <inf> demo: Actual final value: 2000000
+[00:00:40.880,000] <inf> demo: No race this run
+```
--- app/src/main.c
+++ app/src/main.c
@@ -1,68 +1,55 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/printk.h>
 
 LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
-#define STACK_SIZE 1024
-
-#define PRIO_LOW 7
+#define STACK_SIZE  1024
 #define PRIO_MEDIUM 5
-#define PRIO_HIGH 3
-#define PRIO_COOP -1
+#define INCREMENTS  1000000
 
-uint32_t previous = 0;
+static volatile uint32_t counter;
+static struct k_sem done_sem;
+static K_MUTEX_DEFINE(counter_mutex);
 
-void thread_low_fn(void *p1, void *p2, void *p3)
+
+void worker_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        uint32_t now = k_uptime_get_32();
-        printk("T_LOW running. Time elapsed: %u ms\n", now - previous);
-        k_msleep(300);
+    const char *name = k_thread_name_get(k_current_get());
+
+    for (int i = 0; i < INCREMENTS; i++) {
+        k_mutex_lock(&counter_mutex, K_FOREVER);
+        counter++;
+        k_mutex_unlock(&counter_mutex);
     }
+
+    LOG_INF("[%s] finished", name);
+    k_sem_give(&done_sem);
 }
 
-void thread_med_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        uint32_t now = k_uptime_get_32();
-        printk("T_MED running. Time elapsed: %u ms\n", now - previous);
-        k_msleep(200);
-    }
-}
 
-void thread_high_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        uint32_t now = k_uptime_get_32();
-        printk("T_HIGH running. Time elapsed: %u ms\n", now - previous);
-        k_msleep(100);
-    }
-}
-
-void thread_coop_fn(void *p1, void *p2, void *p3)
-{
-    int iteration = 0;
-
-    while (iteration < 5) {
-        printk("Cooperative thread running. Iteration number %d.\n", iteration);
-        iteration++;
-    }
-    k_yield();
-}
-
-K_THREAD_DEFINE(thread_low, STACK_SIZE, thread_low_fn,
-                NULL, NULL, NULL, PRIO_LOW, 0, 0);
-K_THREAD_DEFINE(thread_med, STACK_SIZE, thread_med_fn,
+K_THREAD_DEFINE(worker_a, STACK_SIZE, worker_fn,
                 NULL, NULL, NULL, PRIO_MEDIUM, 0, 0);
-K_THREAD_DEFINE(thread_high, STACK_SIZE, thread_high_fn,
-                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
-K_THREAD_DEFINE(thread_coop, STACK_SIZE, thread_coop_fn,
-                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+K_THREAD_DEFINE(worker_b, STACK_SIZE, worker_fn,
+                NULL, NULL, NULL, PRIO_MEDIUM, 0, 0);
+
 
 int main(void)
 {
-    previous = k_uptime_get_32();
+    LOG_INF("START");
+    k_sem_init(&done_sem,0,2);
+
+    LOG_INF("Expected final value: %d", INCREMENTS *2);
+
+    k_sem_take(&done_sem, K_FOREVER);
+    k_sem_take(&done_sem, K_FOREVER);
+
+    LOG_INF("Actual final value: %u", counter);
+    if(counter == INCREMENTS * 2) {
+        LOG_INF("No race this run");
+    } else {
+        LOG_ERR("Race condition confirmed: lost %d updates", (INCREMENTS * 2) - counter);
+    }
+
     return 0;
 }
 
